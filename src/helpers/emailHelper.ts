a90c9c447d490e875ac2bad445d11cf55bcdf123
Asaduzzama-n <asad_uzzaman@outlook.com>
2025-05-23 11:01:46 +0600

    refactor(emailHelper): update sender name in email configuration
    The sender name in the email configuration has been updated from "Express-Craft" to "hcfinconsults" to reflect the correct branding and improve consistency in email communications.

--- src/helpers/emailHelper.ts
+++ src/helpers/emailHelper.ts
@@ -16,7 +16,7 @@ const transporter = nodemailer.createTransport({
 const sendEmail = async (values: ISendEmail) => {
   try {
     const info = await transporter.sendMail({
-      from: `"Express-Craft" ${config.email.from}`,
+      from: `"hcfinconsults" ${config.email.from}`,
       to: values.to,
       subject: values.subject,
       html: values.html,
